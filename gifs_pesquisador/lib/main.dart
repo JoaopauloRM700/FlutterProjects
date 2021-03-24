80e3d762135ceb441e2dea372250d10ca11a1e8b
Joao Paulo Reis Marques <44990001+JoaopauloRM700@users.noreply.github.com>
2021-03-24 12:24:21 -0400

    GifsSearch

---
+++ gifs_pesquisador/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:gifs_pesquisador/UI/gif_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:gifs_pesquisador/UI/HomePage.dart';
+
+import 'UI/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
